6a5a11ca59589f365cf876cf89051b053e3c4b0f
이시바 <jinho971031@gmail.com>
2022-09-26 14:23:33 +0900

    Add SettingScreen
    세팅 스크린 초기 설정

---
+++ my-app/src/screens/SettingScreen.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+function SettingScreen() {
+  return (
+    <View>
+      <Text>Setting Screen</Text>
+    </View>
+  );
+}
+
+export default SettingScreen;
