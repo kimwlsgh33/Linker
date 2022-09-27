ff73bff73170b1ac51dde6b7008a788737f5eab4
blackm201 <blackm201@naver.com>
2022-09-27 20:44:04 +0900

    add_SignUp_to_develop

--- my-app/src/nav/RootStack.tsx
+++ my-app/src/nav/RootStack.tsx
@@ -3,27 +3,17 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "../screens/HomeScreen";
 import DetailScreen from "../screens/DetailScreen";
 import WelcomeScreen from "../screens/WelcomeScreen";
-<<<<<<< HEAD
 import SignUp from "../screens/SignUp";
-=======
 import LoginScreen from "../screens/LoginScreen";
->>>>>>> 55d3e3ccccf87b44e83aa3eef27464a2e7ebce3a
-
 const Stack = createNativeStackNavigator();
 
 function RootStack() {
   return (
     <Stack.Navigator
-<<<<<<< HEAD
       initialRouteName="SignUp"
       screenOptions={{ headerShown: false }}
     >
-=======
-      initialRouteName="Login"
-      screenOptions={{ headerShown: false }}
-    >
       <Stack.Screen name="Login" component={LoginScreen} />
->>>>>>> 55d3e3ccccf87b44e83aa3eef27464a2e7ebce3a
       <Stack.Screen name="Welcome" component={WelcomeScreen} />
       <Stack.Screen name="Home" component={HomeScreen} />
       <Stack.Screen name="Details" component={DetailScreen} />
