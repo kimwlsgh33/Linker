9c82906602555a3a0a477eb8e7a5dbd99f3e7bbb
blackm201 <blackm201@naver.com>
2022-09-27 21:27:42 +0900

    add_SignUp_f

--- my-app/src/nav/RootStack.tsx
+++ my-app/src/nav/RootStack.tsx
@@ -10,7 +10,7 @@ const Stack = createNativeStackNavigator();
 function RootStack() {
   return (
     <Stack.Navigator
-      initialRouteName="SignUp"
+      initialRouteName="Login"
       screenOptions={{ headerShown: false }}
     >
       <Stack.Screen name="Login" component={LoginScreen} />
