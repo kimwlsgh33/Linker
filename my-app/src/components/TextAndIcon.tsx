49097863b9dfabfe2e2b8a9d169443f64b274abc
swan2203 <swan2203@naver.com>
2022-09-29 21:44:36 +0900

    ScreenSetting
    09-29 21:44

--- my-app/src/components/TextAndIcon.tsx
+++ my-app/src/components/TextAndIcon.tsx
@@ -2,41 +2,51 @@ import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import Icon from "react-native-vector-icons/AntDesign";
 
-function TextAndIcon({ text, iconName, iconSize }) {
+function TextAndIcon({ text, iconName, iconName2, iconSize }) {
   return (
-    <>
+    <View style={styles.container}>
+      <View style={styles.LeftContent}>
+        <View style={styles.IconLeft}>
+          <Icon name={iconName2} size={iconSize} color="#FFFAFA" />
+        </View>
+        <Text style={styles.menutext}>{text}</Text>
+      </View>
       <View style={styles.IconRight}>
         <Icon name={iconName} size={iconSize} color="#FFFAFA" />
       </View>
-      <Text style={styles.menutext}>{text}</Text>
-    </>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    borderWidth: 1,
+    borderColor: "#FFFAFA",
+    justifyContent: "space-between",
+  },
   menutext: {
     fontSize: 15,
     fontWeight: "bold",
     color: "#FFFAFA",
+    // textAlign: "left",
     // flexDirection: "column",
     // alignItems: "flex-start",
     // flexGrow: 1,
     // justify: "space-between",
-    margin: 40,
+    // s
   },
   IconRight: {
-    marginTop: 45,
-    marginRight: 5,
-    marginLeft: 5,
     alignItems: "flex-end",
     // justifyContent: "center",
   },
 
   IconLeft: {
-    marginTop: 45,
-    marginRight: 5,
-    marginLeft: 5,
     alignItems: "flex-start",
+  },
+
+  LeftContent: {
+    flexDirection: "row",
   },
 });
 
