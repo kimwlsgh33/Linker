aaa2698278fcd0f136b2c18f4b20da6ae709ffef
이시바 <jinho971031@gmail.com>
2022-10-06 19:48:03 +0900

    Add SearchBar&Icon Animation
    검색창을 눌렀을때, 아이콘이 바뀌었다는 것을 더 자연스럽게 알려주기 위해
    검색창의 사이즈를 약간 줄이고, 취소 텍스트가 왼쪽에서 오는 애니메이션을 추가.

--- my-app/src/screens/SearchScreen.tsx
+++ my-app/src/screens/SearchScreen.tsx
@@ -1,6 +1,7 @@
 import React, { useRef, useState } from "react";
 import {
   Animated,
+  Dimensions,
   FlatList,
   Keyboard,
   Pressable,
@@ -16,6 +17,8 @@ import EntryIcon from "react-native-vector-icons/Entypo";
 import PostAndReels from "../components/PostAndReels";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useAnimationValue } from "../hooks/useAnimationValue";
+import { useStyle } from "../hooks/useStyle";
+import { interpolate } from "../utils/interpolate";
 
 //================================================================================================
 // data sets
@@ -40,7 +43,10 @@ const postAndReels = new Array(10).fill(0).map((_, i) => ({
 }));
 //================================================================================================
 // animated Pressable 만들기
-const AnimPressable = Animated.createAnimatedComponent(Pressable);
+const AnimTouchableOpacity = Animated.createAnimatedComponent(TouchableOpacity);
+//================================================================================================
+// 화면 크기 구하기
+const { width, height } = Dimensions.get("window");
 
 function SearchScreen() {
   const [paR, setPaR] = useState(postAndReels);
@@ -53,22 +59,42 @@ function SearchScreen() {
       </View>
     );
   }
-
+  const leftAnimValue = useAnimationValue(-40);
   // 뒤에 화면을 대기하고 있다가, 애니메이션을 시작하면서 앞에 화면을 띄워주는 방식
   const zindexAnimValue = useAnimationValue(-1);
   const opacityAnimValue = useAnimationValue(0);
+  // 검색 TextInput을 눌렀을때, 검색창의 width가 줄어드는 애니메이션
+  const inputAnimValue = useAnimationValue(width - 20 - 40);
+  const inputAnimStyle = useStyle({
+    width: inputAnimValue,
+  });
+  const beforeIconAnimStyle = useStyle({
+    opacity: interpolate(opacityAnimValue, [1, 0]),
+    zIndex: interpolate(zindexAnimValue, [1, -1], [-1, 1]),
+  });
 
   const modalAnim = () => {
     // parallel: 동시에 실행
+    // sequence: 순차적으로 실행
     Animated.parallel([
       Animated.timing(zindexAnimValue, {
         toValue: visible ? -1 : 1, // 화면이 보이는 상태에서는(true) -1, 안보이는 상태에서는 1
-        duration: 300,
+        duration: 500,
         useNativeDriver: true,
       }),
       Animated.timing(opacityAnimValue, {
         toValue: visible ? 0 : 1,
         duration: 300,
+        useNativeDriver: true,
+      }),
+      Animated.timing(inputAnimValue, {
+        toValue: visible ? width - 20 - 40 : width - 20 - 50,
+        duration: 300,
+        useNativeDriver: false,
+      }),
+      Animated.timing(leftAnimValue, {
+        toValue: visible ? -40 : 0,
+        duration: 100,
         useNativeDriver: true,
       }),
     ]).start();
@@ -88,16 +114,40 @@ function SearchScreen() {
   return (
     <SafeAreaView edges={["top"]} style={styles.container}>
       <View style={styles.header}>
-        <View style={styles.searchView}>
+        <Animated.View style={[styles.searchView, inputAnimStyle]}>
           <Icon name="search" size={15} color="rgba(0,0,0,0.5)" />
+
           <TextInput
             style={styles.searchInput}
             placeholder="검색"
             onFocus={open}
           />
-        </View>
+        </Animated.View>
 
-        <EntryIcon name="location" size={20} color="rgba(0,0,0,0.5)" />
+        <View style={styles.iconView}>
+          <AnimTouchableOpacity style={beforeIconAnimStyle}>
+            <View>
+              <EntryIcon name="location" size={20} color="rgba(0,0,0,0.5)" />
+            </View>
+          </AnimTouchableOpacity>
+          <AnimTouchableOpacity
+            style={[
+              styles.animIcon,
+              {
+                transform: [
+                  {
+                    translateX: leftAnimValue,
+                  },
+                ],
+                opacity: opacityAnimValue,
+                zIndex: zindexAnimValue,
+              },
+            ]}
+            onPress={close}
+          >
+            <Text>취소</Text>
+          </AnimTouchableOpacity>
+        </View>
       </View>
 
       <FlatList
@@ -108,19 +158,19 @@ function SearchScreen() {
         )}
       />
 
-      <AnimPressable
+      <Animated.View
         style={[
-          StyleSheet.absoluteFill,
+          styles.backgroundView,
           {
-            marginTop: 50,
-            flex: 1,
-            backgroundColor: "rgba(0,0,0,0.5)",
             zIndex: zindexAnimValue,
             opacity: opacityAnimValue,
           },
         ]}
-        onPress={close}
-      ></AnimPressable>
+      >
+        <View>
+          <Text>test</Text>
+        </View>
+      </Animated.View>
     </SafeAreaView>
   );
 }
@@ -138,8 +188,8 @@ const styles = StyleSheet.create({
   },
   header: {
     flexDirection: "row",
+    justifyContent: "flex-start",
     alignItems: "center",
-    justifyContent: "center",
     width: "100%",
     height: 50,
     backgroundColor: "#fff",
@@ -148,24 +198,43 @@ const styles = StyleSheet.create({
     paddingHorizontal: 10,
   },
   searchView: {
-    flex: 1,
     flexDirection: "row",
     alignItems: "center",
     backgroundColor: "#eee",
     marginLeft: 10,
     marginRight: 10,
-    paddingHorizontal: 7,
+    paddingLeft: 7,
     paddingVertical: 2,
     borderRadius: 10,
+    zIndex: 2,
   },
   searchInput: {
     flex: 1,
     height: 30,
     paddingLeft: 5,
   },
+  iconView: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  animIcon: {
+    backgroundColor: "white",
+    position: "absolute",
+    height: 30,
+    width: 30,
+    justifyContent: "center",
+    alignItems: "center",
+  },
   scrollView: {
     width: "100%",
     backgroundColor: "yellow",
+  },
+  backgroundView: {
+    position: "absolute",
+    top: 95,
+    width,
+    height: height,
+    backgroundColor: "white",
   },
 });
 
