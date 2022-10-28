624f9261c076c56278a738bf7ce2e39267067d1c
blackm201 <blackm201@naver.com>
2022-10-28 17:58:37 +0900

    imsi

--- my-app/src/libs/amplify.ts
+++ my-app/src/libs/amplify.ts
@@ -29,7 +29,6 @@ async function signOut() {
   }
 }
 
-<<<<<<< HEAD
 // Confirm Sign Up
 try {
   await Auth.confirmSignUp(username, code);
@@ -61,59 +60,3 @@ try {
 } catch (error) {
   console.log(error);
 }
-=======
-async function uploadImage(filePath) {
-  try {
-    const response = await fetch(filePath);
-    const blob = await response.blob;
-    await Storage.put("testKey", blob, {
-      contentType: "image.jpeg",
-    });
-  } catch (err) {
-    alert("업로드 실패 : " + err);
-  }
-}
-
-// Create
-type CreatePostProps = {
-  userID: string;
-  title: string;
-  tagName: string;
-  text: string;
-  link: string;
-};
-async function createPost({
-  userID,
-  title,
-  tagName,
-  text,
-  link,
-}: CreatePostProps) {
-  const tag = await DataStore.save(
-    new Tag({
-      tagname: tagName,
-    })
-  );
-  await DataStore.save(
-    new Post({
-      title,
-      tagID: tag.id,
-      text,
-      link,
-      userID,
-    })
-  );
-}
-
-// Update
-/* Models in DataStore are immutable. To update a record you must use the copyOf function
- to apply updates to the item’s fields rather than mutating the instance directly */
-
-// Delete
-// const modelToDelete = await DataStore.query(Post, 123456789);
-// DataStore.delete(modelToDelete);
-
-// Query
-// const posts = await DataStore.query(Post);
-// console.log(posts);
->>>>>>> 60c4501136423448532c4e663e703067213ebacd
